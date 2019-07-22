13b7f48f8de34c1f3447001e5fd9bd287267144e
Shane Lonergan <sptlonergan@gmail.com>
2019-07-22 01:59:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  removed_element = array.pop
+end
+
+def pop_with_args(array)
+  removed_elements = array.pop(2)
+end 
+
